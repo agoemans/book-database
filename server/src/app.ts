84e4c3d90084bd3e4e5820050e912cfbfeeaf4e1
Amy <kewlboontjies@gmail.com>
2021-07-31 12:37:49 +0200

    Added docker file

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,18 +8,16 @@ const app = express();
 
 const startApp = () => {
         app.use(cors({
-        origin: 'http://localhost:8080',
+        origin: '*',
         optionsSuccessStatus: 200
     }));
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-    if (process.env.NODE_ENV === 'development') {
-        app.listen(process.env.PORT, () => {
-            return console.log(`server is listening on ${process.env.PORT}`);
-        });
-    }
+    app.listen(process.env.PORT, () => {
+        return console.log(`server is listening on ${process.env.PORT}`);
+    });
 };
 
 const createRoutes = () => {
